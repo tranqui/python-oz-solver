b6c202c363b9981034bedcc89eb0dc55d8fd0918
Joshua F. Robinson <joshuarrr@protonmail.com>
2026-01-20 13:45:54 +0000

    Wrap in object-oriented interface

--- pitzer.py
+++ pitzer.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
+from types import SimpleNamespace
+
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import pi as π
 from numpy import exp, sqrt
+from scipy.constants import N_A as NA, Boltzmann as kB, e, epsilon_0 as ε0
+from scipy.constants import atmosphere as atmospheric_pressure
 
+import chemicals
 from rdkit import Chem
-from rdkit.Chem.Descriptors import ExactMolWt
+from rdkit.Chem.Descriptors import MolWt
+
+# Water constants
+
+water = Chem.MolFromSmiles('O')
+water_molar_mass = 1e-3 * MolWt(water) # [kg/mol]
+
 
 # Standard Pitzer model, revised
 # Values of β0, β1, Cϕ from Table 1 in J. Chem. Eng. Data 33, 177-84 (1988)
@@ -31,46 +42,31 @@ for name in pitzer_coeffs:
     for s in split:
         if s not in ions: ions += [s]
 
-for ion in ions:
-    mol = Chem.MolFromSmiles(f'[{ion}]')
-    print(f'{ion:>4}: {ExactMolWt(mol):>8.4f} g/mol')
 
-print()
+class PitzerModel:
+    def __init__(self, name):
+        self.regular_coeffs = pitzer_coeffs[name]
+        self.modified_coeffs = modified_pitzer_coeffs[name]
 
-T = 298 # temperature
-ρw, εr = 0.997e3, 78.4 # water properties at 298 K
-NA, kB, e, ε0 = 6.022e23, 1.38e-23, 1.602e-19, 8.854e-12 # fundamental constants
+    def __call__(self, T, m, modified=False,
+                 pressure=atmospheric_pressure):
 
-lB = e**2 / (4*π*εr*ε0*kB*T) # Bjerrum length in water
+        ρw = chemicals.iapws.iapws97_rho(T, pressure)
+        εr = chemicals.permittivity.permittivity_IAPWS(T, ρw)
+        lB = e**2 / (4*π*εr*ε0*kB*T) # Bjerrum length in water
 
-Aϕ = 1/3 * sqrt(2*π*NA*ρw) * lB**(3/2) # Debye-Hückel coefficient
-print(f'Aϕ (calculated) = {Aϕ}')
+         # Debye-Hückel coefficient
+        Aϕ = 1/3 * sqrt(2*π*NA*ρw) * lB**(3/2)
+        # Aϕ = 0.3915 # to match above Ind. Eng. Chem. Res. 41(5), 1031-7 (2002)
 
-# Aϕ = 0.3915 # to match above Ind. Eng. Chem. Res. 41(5), 1031-7 (2002)
-# print(f'Aϕ (assumed) = {Aϕ}')
-
-print(f'Πref / molality (1:1 electrolyte) = {2*NA*kB*T*ρw*1e-5} × 10^5 Pa.kg/m³')
-print(f'Πref / (∑i mi) = RT ρw = {NA*kB*T*ρw*1e-5} × 10^5 Pa.kg/m³')
-
-# For 1:1 electrolytes the stoichiometry coefficients and valencies are all unity
-
-m = np.insert(np.geomspace(1e-3, 10, 51), 0, 0.0) # prepend 0.0 to logspace
-
-rootI = sqrt(m) # sqrt of ionic strength
-
-for name in pitzer_coeffs:
-    for modified in [False, True]:
-
+        rootI = sqrt(m)
         if modified:
-
-            b, β0, CMX = modified_pitzer_coeffs[name]
+            b, β0, CMX = self.modified_coeffs
             Bϕ = β0
             Cϕ = 2 * CMX
-
         else:
-
+            β0, β1, Cϕ = self.regular_coeffs
             α, b = 2, 1.2
-            β0, β1, Cϕ = pitzer_coeffs[name]
             Bϕ = β0 + β1 * exp(-α*rootI)
 
         Φ = 1 - Aϕ*rootI/(1 + b*rootI) + Bϕ*m + Cϕ*m**2
@@ -87,27 +83,42 @@ for name in pitzer_coeffs:
 
         result = np.vstack((m, Πref, Π, Φ)).transpose()
 
-        molar_mass_water = 18.01528e-3 # kg/mol
+        x = m / (m + 1/water_molar_mass) # mole fraction
 
-        x = m / (m + 1/molar_mass_water)
-        x = m
-        y = Π
-        # y = Φ
+        return SimpleNamespace(m=m, Φ=Φ, Πref=Πref, Π=Π, x=x)
 
-        if modified:
-            plt.plot(x, y, ':', c=pl.get_color())
-        else:
-            label = name.replace('+', '').replace('-', '')
-            label = rf'$\ce{{{label}}}$'
-            pl, = plt.plot(x, y, label=label)
 
-plt.legend(loc='best')
-plt.ylabel('osmotic pressure $\Pi$ / bar')
-# plt.ylabel('osmotic coefficient $\phi$')
-# plt.xlabel(r'mole fraction $x_z \equiv x_+ + x_-$')
-plt.xlabel(r'molality $m$ / $\si{\mol\per\kilogram}$')
+if __name__ == '__main__':
 
-plt.ylim([0, 500])
-plt.xlim([0, 10])
+    for ion in ions:
+        mol = Chem.MolFromSmiles(f'[{ion}]')
+        print(f'{ion:>4}: {MolWt(mol):>8.4f} g/mol')
 
-plt.show()
+    T = 298 # temperature
+    m = np.insert(np.geomspace(1e-3, 10, 51), 0, 0.0) # prepend 0.0 to logspace
+
+    fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(3.375, 3.375))
+
+    for name in pitzer_coeffs:
+        model = PitzerModel(name)
+
+        for modified in [False, True]:
+            result = model(T, m, modified)
+
+            if modified:
+                ax1.plot(result.x, result.Φ, ':', c=pl.get_color())
+                ax2.plot(result.m, result.Π, ':', c=pl.get_color())
+            else:
+                label = name.replace('+', '').replace('-', '')
+                label = rf'$\ce{{{label}}}$'
+                pl, = ax1.plot(result.x, result.Φ, label=label)
+                ax2.plot(result.m, result.Π)
+
+
+    ax1.set_xlabel(r'mole fraction $x_z \equiv x_+ + x_-$')
+    ax1.set_ylabel(r'osmotic\\coefficient $\phi$')
+    ax1.legend(loc='best')
+    ax2.set_xlabel(r'molality $m$ / $\si{\mol\per\kilogram}$')
+    ax2.set_ylabel(r'osmotic\\pressure $\Pi$ / bar')
+
+    plt.show()
